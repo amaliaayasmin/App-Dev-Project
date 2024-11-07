544a13e75e5793241233b0e2c00786e290fed340
Amalia Yasmin <amaliayasmin@graduate.utm.my>
2024-11-07 15:50:17 +0800

    Logo Images

--- resources/views/include/header.blade.php
+++ resources/views/include/header.blade.php
@@ -1,5 +1,8 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
+
+    <!--Left Side : Logo & Navigation Links -->
+     
     <a class="navbar-brand" href="#">Oh My Merits!</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
