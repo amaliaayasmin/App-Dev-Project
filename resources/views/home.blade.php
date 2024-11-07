9dd58cc8277dd74909396b7e5da6b056153e54b9
Amalia Yasmin <amaliayasmin@graduate.utm.my>
2024-11-07 17:31:24 +0800

    Add Header in Layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,8 +7,14 @@
     <title>Home Page</title>
 </head>
 <body>
+    @extends('layout')
+    @section('title', 'Home Page')
+    @section('content')
+
     <h1>Welcome to the Home Page</h1>
     <p>You have successfully logged in!</p>
-    <!-- Add more content as needed -->
+
+    @endsection
+
 </body>
 </html>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -9,7 +9,7 @@
   <body>
     @include('include.header')
     @yield('content')
-    <h1></h1>
+    
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" ></script>
   </body>
 </html>
