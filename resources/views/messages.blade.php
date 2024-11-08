6a7a0aec80db28e7f5c77d7a4dfac35490776561
Amalia Yasmin <amaliayasmin@graduate.utm.my>
2024-11-08 18:18:28 +0800

    System Settings (NEW)
    System Settings by Developer 1 (Sudashan Rao)

--- resources/views/messages.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout')
-
-@section('title', 'Messages')
-
-@section('content')
-    <h1>Messages</h1>
-    <p>This is the Messages page.</p>
-@endsection
--- resources/views/saved-events.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout')
-
-@section('title', 'Saved Events')
-
-@section('content')
-    <h1>Saved Events</h1>
-    <p>This is the Saved Events page.</p>
-@endsection
